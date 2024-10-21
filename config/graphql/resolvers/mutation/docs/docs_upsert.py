8ae0f96b797409534720f5814532012d18aaa9e1
Vukovic Nikola <admin@nikolav.rs>
2024-10-21 09:48:42 +0200

    Mon Oct 21 07:48:42 UTC 2024

--- config/graphql/resolvers/mutation/docs/docs_upsert.py
+++ config/graphql/resolvers/mutation/docs/docs_upsert.py
@@ -17,32 +17,34 @@ def resolve_docsUpsert(_obj, _info, topic, data, id = None):
 
   try:
     doc = db.session.scalar(
-      db.select(Docs)
-        .join(Docs.tags)
-        .where(
-          Tags.tag == topic, 
-          Docs.id  == id))
+      db.select(
+        Docs
+      ).join(
+        Docs.tags
+      ).where(
+        topic == Tags.tag, 
+        id    == Docs.id))
   
-  except:
-    pass
-
-  else:
     if doc:
       # update
       doc.data = data
+      
     else:
       # create
       doc = Docs(data = data)
       tagTopic = Tags.by_name(topic, create = True)
       tagTopic.docs.append(doc)
-        
-    try:
-      db.session.commit()
-      
-    except:
-      pass
+
+
+    db.session.commit()
+
+
+  except Exception as err:
+    raise err
+
     
-    else:
-      io.emit(f'{IOEVENT_JsonData}{topic}')
+  else:
+    io.emit(f'{IOEVENT_JsonData}{topic}')
+  
       
   return doc.dump()
