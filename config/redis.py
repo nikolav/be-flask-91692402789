801edc74557f8c052dec48272e36c4795f27c95e
Vukovic Nikola <admin@nikolav.rs>
2024-11-03 14:01:36 +0100

    Sun Nov  3 13:01:36 UTC 2024

--- config/redis.py
+++ config/redis.py
@@ -4,22 +4,27 @@ from flask_redis import FlaskRedis
 
 print('@redis:init')
 
+initialized = False
+
 client = None
 error  = None
 
 def redis_init(app):
   global client
   global error
+  global initialized
 
-  if client:
-    return (None, client)
-  
-  try:
-    client = FlaskRedis()
-    client.init_app(app)
-  
-  except Exception as err:
-    error = err
+  if not initialized:  
+
+    try:
+      client = FlaskRedis()
+      client.init_app(app)
+    
+    except Exception as err:
+      error = err
+    
+    initialized = True
+    
   
   return (error, client)
 
