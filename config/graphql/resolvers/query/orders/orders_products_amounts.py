8eccd84d51417d3731e50662d586a3f6a1e5ba1a
Vukovic Nikola <admin@nikolav.rs>
2024-12-22 13:39:41 +0100

    Sun Dec 22 12:39:41 UTC 2024

--- config/graphql/resolvers/query/orders/orders_products_amounts.py
+++ config/graphql/resolvers/query/orders/orders_products_amounts.py
@@ -6,22 +6,33 @@ from models.assets import Assets
 from models.orders import Orders
 from models import ln_orders_products
 from schemas.serialization import SchemaSerializeAssets
+from schemas.serialization import SchemaSerializeOrders
 
 
-# ordersProductsAmounts(ooid: ID!): [Asset!]!
+# ordersProductsAmounts(ooid: ID!): OrderItems!
 @query.field('ordersProductsAmounts')
 def resolve_ordersProductsAmounts(_obj, _info, ooid):
-  q = db.select(
-      Assets, 
-      ln_orders_products.c.amount,
-    ).join(
-      ln_orders_products
-    ).join(
-      Orders
-    ).where(
-      ooid == Orders.id)
+  # {order:IOrders; items:{amount:number; product:Assets}[]}
+
+  r = { 'order': None, 'items': [] }
+  o = db.session.get(Orders, ooid)
+
+  if o:
+    q = db.select(
+        Assets, 
+        ln_orders_products.c.amount,
+      ).join(
+        ln_orders_products
+      ).join(
+        Orders
+      ).where(
+        ooid == Orders.id)
+    
+    r['order'] = SchemaSerializeOrders().dump(o)
+    r['items'] = [{ 
+                    'amount' : amount,
+                    'product': SchemaSerializeAssets(exclude = ('assets_has', 'author',)).dump(a),
+                  } for a, amount in db.session.execute(q)]
   
-  return [{ 
-            'product': SchemaSerializeAssets(exclude = ('assets_has', 'author',)).dump(a),
-            'amount' : amount,
-          } for a, amount in db.session.execute(q)]
+  return r
+
