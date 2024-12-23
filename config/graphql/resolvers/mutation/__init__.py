a0b87b566059a199c082be7a5611e37476cba849
Vukovic Nikola <admin@nikolav.rs>
2024-12-23 19:12:01 +0100

    Mon, Dec 23, 2024  6:12:01 PM

--- config/graphql/resolvers/mutation/__init__.py
+++ config/graphql/resolvers/mutation/__init__.py
@@ -41,4 +41,8 @@ from .docs_reports import reports_config
 from .redis import cache_redis_commit
 
 from .orders import catalog_order_add
+from .orders import catalog_remove
+from .orders import catalog_manage_tags
+
+
 
--- models/orders.py
+++ models/orders.py
@@ -1,4 +1,6 @@
 
+from enum import Enum
+
 from typing         import Optional
 from typing         import List
 
@@ -22,6 +24,15 @@ from . import assetsTable
 from . import ln_orders_tags
 from . import ln_orders_products
 
+from .tags import Tags
+
+
+class OrdersIOEvents(Enum):
+  IOEVENT_ORDERS_CONFIGRED_prefix = 'IOEVENT_ORDERS_CONFIGRED:b6c6caf1-9be2-57a5-9ba0-6254e59d6909:'
+
+class OrdersTags(Enum):
+  TAG_ORDERS_SHAREABLE_GLOBALY = 'TAG_ORDERS_SHAREABLE_GLOBALY:61cde3f6-cdf8-5769-bf11-93b91f4ff49d'
+
 
 class Orders(MixinTimestamps, MixinIncludesTags, MixinByIds, MixinExistsID, MixinFieldMergeable, db.Model):
   __tablename__ = ordersTable
@@ -44,3 +55,32 @@ class Orders(MixinTimestamps, MixinIncludesTags, MixinByIds, MixinExistsID, Mixi
   tags     : Mapped[List['Tags']]   = relationship(secondary = ln_orders_tags,     back_populates = 'orders')
   products : Mapped[List['Assets']] = relationship(secondary = ln_orders_products, back_populates = 'orders')
   
+
+  # public
+  def tags_add(self, *tags, _commit = True):
+    changes = 0
+
+    for tname in filter(lambda p: not self.includes_tags(p), tags):
+      tp = Tags.by_name(tname, create = True, _commit = _commit)
+      tp.orders.append(self)
+      changes += 1
+    
+    if (0 < changes) and (True == _commit):
+      db.session.commit()
+    
+    return changes
+  
+  
+  # public
+  def tags_rm(self, *tags, _commit = True):
+    changes = 0
+
+    for tname in filter(lambda p: self.includes_tags(p), tags):
+      tp = Tags.by_name(tname, create = True, _commit = _commit)
+      tp.orders.remove(self)
+      changes += 1
+    
+    if (0 < changes) and (True == _commit):
+      db.session.commit()
+    
+    return changes
